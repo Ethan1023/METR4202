7003e791ac02dc54ce638f79ea2343d90441aece
BuildTools <unconfigured@null.spigotmc.org>
2022-09-21 12:51:47 +1000

    Jittery movement in a straight line :)

--- scripts/joint_controller_ros.py
+++ scripts/joint_controller_ros.py
@@ -7,6 +7,7 @@ import rospy
 import numpy as np
 from sensor_msgs.msg import JointState
 from inverse_kinematics import inv_kin
+from forward_kinematics import derivePoE, PoE
 import random
 
 class JointController:
@@ -29,6 +30,7 @@ class JointController:
         self.joint_velocities = ()
         self.joint_efforts = ()
         self.names = ('joint_1', 'joint_2', 'joint_3', 'joint_4')
+        self.Tsb, self.screws = derivePoE()
 
     def joint_state_callback(self, joint_state):
         self.joint_names = joint_state.name  # TODO - make this only be set once, or hard code?
@@ -45,6 +47,30 @@ class JointController:
             joint_state.velocity = desired_vel
         self.joint_pub.publish(joint_state)
 
+    def get_current_pos(self):
+        if len(self.joint_names) == 0:
+            return None, None
+        thetas = []
+        for name in self.names:
+            index = -1
+            for theta, new_name in zip(self.joint_positions, self.joint_names):
+                print(name, new_name)
+                if name == new_name:
+                    thetas.append(theta)
+        T1 = PoE(self.Tsb, self.screws, thetas)
+        print(T1)
+
+    def end_effector_publisher(self, desired_coords, desired_pitch, desired_vel=None):
+        possible = inv_kin(desired_coords, desired_pitch, check_possible=True)
+        print(desired_coords)
+        if possible:
+            thetas = inv_kin(desired_coords, desired_pitch)
+            print(f'thetas = {thetas}')
+            self.joint_state_publisher(thetas, desired_vel)
+
+    def go_to_pos(self, desired_coords, desired_pitch, steps=100):
+        current_coords = None
+
 def main():
     # Create ROS node
     jc = JointController()
@@ -59,18 +85,38 @@ def test(jc):
     print(f'efforts = {jc.joint_efforts}')
     print()
     desired_pos = (0*np.pi/180, 0*np.pi/180, 0*np.pi/180, 0*np.pi/180)
-    desired_vel = (20*np.pi/180, 20*np.pi/180, 20*np.pi/180, 20*np.pi/180)
-    jc.joint_state_publisher(desired_pos, desired_vel)
-    rate = rospy.Rate(5)
+    HEIGHT = 0.22
+    #desired_coords = [0.1, 0, HEIGHT]
+    #desired_pitch = 0
+    DIST = 0.1
+    desired_coords = [DIST, 0, 0.1]
+    desired_pitch = -np.pi/2
+    speed = 50
+    desired_vel = (speed*np.pi/180, speed*np.pi/180, speed*np.pi/180, speed*np.pi/180)
+    #jc.joint_state_publisher(desired_pos, desired_vel)
+    jc.end_effector_publisher(desired_coords, desired_pitch, desired_vel)
+    rate = rospy.Rate(100)
+    i = 0
     while not rospy.is_shutdown():
-        ang = 20
-        desired_pos = ((random.random()-0.5)*ang*np.pi/180, (random.random()-0.5)*ang*np.pi/180, (random.random()-0.5)*ang*np.pi/180, (random.random()-0.5)*ang*np.pi/180)
-        jc.joint_state_publisher(desired_pos, desired_vel)
+        #ang = 20
+        #desired_pos = ((random.random()-0.5)*ang*np.pi/180, (random.random()-0.5)*ang*np.pi/180, (random.random()-0.5)*ang*np.pi/180, (random.random()-0.5)*ang*np.pi/180)
+        #jc.joint_state_publisher(desired_pos, desired_vel)
+        #desired_coords = [min(0.2,0.1+i/500), 0, HEIGHT]
+        #if i > 50:
+        #    desired_coords = [max(0.1,0.2-(i-50)/500), 0, HEIGHT]
+        desired_coords = [DIST, 0, max(0, 0.1-i/500)]
+        if i > 50:
+            desired_coords = [DIST, 0, min(0.1,0+(i-50)/500)]
+        jc.end_effector_publisher(desired_coords, desired_pitch, desired_vel)
+        jc.get_current_pos()
         print(f'names = {jc.joint_names}')
         print(f'positions = {jc.joint_positions}')
         print(f'velocities = {jc.joint_velocities}')
         print(f'efforts = {jc.joint_efforts}')
         print()
+        i += 0.1
+        if i > 100:
+            i = 0
         rate.sleep()
 
 if __name__ == '__main__':
