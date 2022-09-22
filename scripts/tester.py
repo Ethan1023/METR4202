15b71d90fc77b174604a240dfe17b4b68e2a664e
Ethan1023 <39666476+Ethan1023@users.noreply.github.com>
2022-09-22 21:05:10 +1000

    Test case for joint velocity calculation

--- scripts/tester.py
+++ scripts/tester.py
@@ -1,10 +1,102 @@
 import numpy as np
-from inverse_kinematics import inv_kin
-from forward_kinematics import derivePoE, PoE
+from inverse_kinematics import inv_kin, atan2
+from forward_kinematics import derivePoE, PoE, derive_inv_jac, calc_frame1_vel
 from modern_robotics import TransToRp
 from constants import L1, L2, L3, L4
 
 def main():
+    test_many_jacobian()
+    #test_all_kinematics()
+
+def test_many_jacobian():
+    '''
+    Compare jacobian and numerical methods with same initial positions and velocities through numerical integration
+    '''
+    thetas = np.array([0, 0, np.pi/2, 0])
+    thetas = inv_kin(np.array([0.05, 0, 0.1]), -np.pi/2)
+    print(thetas*180/np.pi)
+    coords_vel = np.array([0.1, 0.1, 0.1])
+    #coords_vel = np.array([0, 0, 0])
+    #coords_vel = np.array([0, 0, 0])
+    #pitch_vel = 1
+    pitch_vel = 0.1
+    dt = 0.001
+    time = 0.1
+    Tsb, screws = derivePoE()
+    thetas_analytical = thetas
+    thetas_numerical = thetas
+    print(f'Original thetas = {np.round(thetas*180/np.pi,3)}')
+    steps = int(time/dt)
+    print(steps)
+    thetas_actual = apply_velocity_numerical(thetas, coords_vel, pitch_vel, time, Tsb=Tsb, screws=screws)
+    for i in range(steps):
+        thetas_analytical = apply_velocity_jacobian(thetas_analytical, coords_vel, pitch_vel, dt, ignore=True, Tsb=Tsb, screws=screws, printing=False)
+        thetas_numerical = apply_velocity_numerical(thetas_numerical, coords_vel, pitch_vel, dt, Tsb=Tsb, screws=screws, printing=False)
+        if i % int(steps/10) == 0:
+            error = np.sum(np.abs(thetas_analytical-thetas_numerical))
+            print(f'{i/steps*100}%, error={error*180/np.pi}')
+    print(f'Analytical new thetas = {np.round(thetas_analytical*180/np.pi,3)}')
+    print(f'Numerical new thetas = {np.round(thetas_numerical*180/np.pi,3)}')
+    print(f'Actual new thetas = {np.round(thetas_actual*180/np.pi,3)}')
+    error = np.sum(np.abs(thetas_analytical-thetas_numerical))
+    print(f'Cumulative absolute error = {error * 180/np.pi} deg')
+    print(f'This should be constant with dt as dt -> infinity:')
+    print(f'Cumulative absolute error per dt = {error/dt * 180/np.pi} deg/s')
+        
+
+def apply_velocity_jacobian(thetas=None, coords_vel=None, pitch_vel=1.0, dt=0.001, printing=False, ignore=True, Tsb=None, screws=None):
+    '''
+    Update joint angles through jacobian
+    '''
+    if thetas is None:
+        thetas = np.array([0, 0, np.pi/2, 0])
+    if coords_vel is None:
+        coords_vel = np.array([1, 1, 1])
+    if Tsb is None or screws is None:
+        Tsb, screws = derivePoE()
+    T04 = PoE(Tsb, screws, thetas)  # Find end effector configuration
+    if printing:
+        print(f'T04=\n{np.round(T04,3)}')
+    J1inv = derive_inv_jac(thetas, printing=False)  # Find inverse jacobian in L1 space
+    if printing:
+        print(f'J1inv=\n{np.round(J1inv,3)}')
+    L1_4DOF_vel = calc_frame1_vel(thetas, coords_vel, pitch_vel, Tsb=Tsb, screws=screws, T04=T04, printing=False, ignore=ignore)  # Find velocity in L1 space
+    if printing:
+        print(f'L1_4DOF_vel={np.round(L1_4DOF_vel,3)}')
+    thetas_vel = np.matmul(J1inv, L1_4DOF_vel)  # Find angular velocities
+    if printing:
+        print(f'Joint vel={np.round(thetas_vel,3)}')
+    thetas_2_jac = thetas + thetas_vel * dt
+
+    return thetas_2_jac
+
+def apply_velocity_numerical(thetas=None, coords_vel=None, pitch_vel=1.0, dt=0.001, printing=False, Tsb=None, screws=None):
+    '''
+    Update joint angles 'numerically'
+    '''
+    if thetas is None:
+        thetas = np.array([0, 0, np.pi/2, 0])
+    if coords_vel is None:
+        coords_vel = np.array([1, 1, 1])
+    if Tsb is None or screws is None:
+        Tsb, screws = derivePoE()
+    T04 = PoE(Tsb, screws, thetas)
+    if printing:
+        print(f'T04=\n{np.round(T04,3)}')
+
+    _, coords = TransToRp(T04)  # original coords
+    pitch = np.pi/2 - np.sum(thetas[1:])  # original pitch
+
+    coords_2 = coords + coords_vel * dt
+    pitch_2 = pitch + pitch_vel * dt
+    thetas_2_num = inv_kin(coords_2, pitch_2)
+    if printing:
+        print(f'Numerical joint vel={np.round((thetas_2_num-thetas)/dt,3)}')
+    
+    return thetas_2_num
+
+
+def test_all_kinematics():
     L234 = L2 + L3 + L4
     resolution = 10
     coords = []
