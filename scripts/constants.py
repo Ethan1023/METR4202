5f925711c60fe78c8d169c5c1b265a2d6e92df6d
Deren Teo <teo.deren@icloud.com>
2022-10-11 11:46:47 +1000

    Correct camera transform

--- scripts/constants.py
+++ scripts/constants.py
@@ -10,10 +10,10 @@ L3 = 0.0945
 L4 = 0.083
 
 # TRANSFORMATIONS
-T_CAMERA_TO_FIXED = [[ 0.000, -1.000,  0.000, 0.206],
-                     [-1.000,  0.000,  0.000, 0.000],
-                     [ 0.000,  0.000, -1.000, 0.460],
-                     [ 0.000,  0.000,  0.000, 1.000]]
+T_CAMERA_TO_FIXED = [[0.000, 1.000,  0.000, 0.206],
+                     [1.000, 0.000,  0.000, 0.000],
+                     [0.000, 0.000, -1.000, 0.460],
+                     [0.000, 0.000,  0.000, 1.000]]
 
 # SAFETY
 MAX_JOINT_VEL = (5, 3, 5, 10)
@@ -35,11 +35,11 @@ EMPTY_HEIGHT = 0.05
 CARRY_HEIGHT = 0.08
 
 #TEST EQUIPMENT DIMENSIONS
-H_BLOCK = 0.032 
+H_BLOCK = 0.032
 RAD_BELT = 0.12
 L_BASE = 0.29
 W_BASE = 0.2
-H_BASE = 0.05 
+H_BASE = 0.05
 L_FENCE = 0.2
 W_FENCE = 0.005
 H_FENCE = 0.05
