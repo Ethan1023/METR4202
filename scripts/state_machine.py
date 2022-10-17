3a633a09d3c10fb6be7eca64758d552c9c77c508
gabriellavidgen <gabvidgen@gmail.com>
2022-10-18 09:55:50 +1000

    Changed input arguments to be list of xy coords instead of 4x4 matrix

--- scripts/state_machine.py
+++ scripts/state_machine.py
@@ -3,27 +3,20 @@ import numpy as np
 def main():
     pass
 
-def block_distances(pos_list):
+def block_distances(coords_list):
     '''
     Calculates the distances (norm) of each fo the blocks from the robot base.
-    args: pos (4x4 transformation matrix)
+    args: list of x and y coordinates
     returns: list of block distances
     '''
-    coords_list = []
-    for pos in pos_list:
-        #get 2x1 position vector from 4x4 matrix (x and y coords)
-        coords = pos[:2, 3]
-        coords_list.append(coords) 
-
     block_distances = []
     for coords in coords_list:
         #unpack position vector into coordinates
-        x, y = pos
+        x, y = coords
         #calculate the distance
         block_distance = np.hypot(x,y)
         #create list of distances
         block_distances.append(block_distance)
-        
     return block_distances
 
 
