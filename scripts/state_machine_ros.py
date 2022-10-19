a4b9d7b6ab742b3dce1af3a3676fb3dda7248b6b
RPi User <jemmyjiang15@hotmail.com>
2022-10-19 16:33:09 +1000

    small changes

--- scripts/state_machine_ros.py
+++ scripts/state_machine_ros.py
@@ -345,7 +345,7 @@ class StateMachine:
         # print(f'Requestself.desired_idlour')
         # self.request_colour()
         # print(f'Colour = {self.detected_colour}')
-        return STATE_RESET # TEMPORARY, FIX THIS - TODO
+        return STATE_PLACE # TEMPORARY, FIX THIS - TODO
 
     def state_place(self):
         # Get destination from state_colour
